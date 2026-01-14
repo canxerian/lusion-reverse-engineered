c5da3700d837f7717bf4cb1be637ac0ef426025e
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2026-01-14 14:11:36 +0000

    videoPanelVert - revisit the wobble animation

--- src/shaders/videoPanelVert.glsl
+++ src/shaders/videoPanelVert.glsl
@@ -8,7 +8,12 @@ uniform vec4 endRect;
 
 varying vec2 vUv;
 
-// Takes VideoPanelShader rect (see VideoPanelShader.rectToVec4) and creates a position based on percentage a
+vec2 rotateLocal(vec2 v, float a) {
+    float s = sin(a);
+    float c = cos(a);
+    return vec2(v.x * c - v.y * s, v.x * s + v.y * c);
+}
+
 vec2 getRectPos(vec4 rect, vec2 uv) {
     vec2 pos;
     pos.x = mix(rect.x, rect.x + rect.w, uv.x);
@@ -17,27 +22,24 @@ vec2 getRectPos(vec4 rect, vec2 uv) {
 }
 
 void main() {
-    vec3 pos = position;
-
-    float rotateStepEdgeCurve = 1.0 - sin(animateProgress * PI) * 2.0;    // multiply by 3 so that when animateProgress = 1, step edge contains more of the uv
-    float startEndCurve = smoothstep(0.2, 1.0, animateProgress);
-    float rotateCurve = (smoothstep(0.1, 0.4, animateProgress) - smoothstep(0.6, 0.9, animateProgress)) * 0.4;
-    float translateCurve = smoothstep(0.0, 0.2, animateProgress) - smoothstep(0.2, 0.8, animateProgress);
+    float transitionWeight = 1. - (pow(uv.x * uv.x, 0.75) + pow(uv.y, 1.5)) / 2.;
+    
+    float localProgress = smoothstep(transitionWeight * 0.3, 0.7 + transitionWeight * 0.3, animateProgress);
 
     vec2 videoPanelStartPos = getRectPos(startRect, uv);
     vec2 videoPanelEndPos = getRectPos(endRect, uv);
+    vec2 posXY = mix(videoPanelStartPos, videoPanelEndPos, localProgress);
 
-    float rotateMask = smoothstep(rotateStepEdgeCurve, 1.0, uv.x);
-    rotateMask *= smoothstep(rotateStepEdgeCurve, 1.0, uv.y);
+    float width = mix(startRect.w, endRect.w, localProgress);
+    posXY.x += mix(width, 0., cos(localProgress * PI * 2.) * 0.5 + 0.5) * 0.1;
 
-    float translateMask = smoothstep(0.6, 1.0, uv.x);
-    translateMask *= smoothstep(0.5, 1.0, uv.y);
+    // This creates a tilt that starts at 0, peaks in the middle, and returns to 0
+    float rot = (smoothstep(0., 1., localProgress) - localProgress) * -0.5;
+    
+    // Apply rotation around the center of the current interpolated position
+    posXY = rotateLocal(posXY, rot);
 
-    pos.xy = mix(videoPanelStartPos, videoPanelEndPos, startEndCurve);
-    pos.xy = rotate(pos.xy, rotateCurve * rotateMask);
-    pos.x *= 1.0 + 0.3 * translateCurve * translateMask;
-
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(posXY, 0.0, 1.0);
 
     vUv = uv;
 }
